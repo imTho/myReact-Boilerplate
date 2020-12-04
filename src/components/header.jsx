f8424bc972a526524aa287a901142caba294e331
Theo <theo.eschlimann@gmail.com>
2020-12-04 14:55:44 +0100

    CSS Changes

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
-import { withRouter } from "react-router-dom";
 
 // Define reducer
 
@@ -8,21 +7,19 @@ const Header = () => {
   return (
     <div className="header">
       <div className="container">
-        <div className="row v-center space-between">
-          <div className="logo">
-            <NavLink to="/" exact>
-              LOGO
-            </NavLink>
-          </div>
-          <div className="nav-toggle">
-            <div className="hamburger-menu">
-              <span></span>
-              <span></span>
-            </div>
+        <div className="logo">
+          <NavLink to="/" exact>
+            LOGO
+          </NavLink>
+        </div>
+        <div className="nav-toggle">
+          <div className="hamburger-menu">
+            <span></span>
+            <span></span>
           </div>
         </div>
       </div>
     </div>
   );
 };
-export default withRouter(Header);
+export default Header;
